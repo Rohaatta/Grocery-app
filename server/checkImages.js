57dfd6c4aa986d96b42c13923060105714927a58
Rohaatta <rohaashir82@gmail.com>
2026-06-05 02:23:29 +0500

    edited the name

--- server/checkImages.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-
-await mongoose.connect(process.env.MONGODB_URI);
-const products = await mongoose.connection.db.collection('products').find({'image.0': {$exists: true}}).toArray();
-products.forEach(x => console.log(x.name, ':', x.image[0]));
-await mongoose.disconnect();
-process.exit();
